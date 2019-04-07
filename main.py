8b7ce4eaf3d74621e3661e6e1ab5e58a225e0d49
Austin Whitelaw <apwhitelaw@hotmail.com>
2019-04-07 03:13:07 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+# Simple Twitter bot that retrieves Bitcoin price information and tweets it out, every 5 minutes.
+
+import tweepy
+import json
+import requests
+import keys
+import sched, time
+
+class TwitterBot:
+
+    consumer_key = keys.consumer_key
+    consumer_secret = keys.consumer_secret
+    access_token = keys.access_token
+    access_token_secret = keys.access_token_secret
+
+    api = tweepy.API()
+    api_url = "https://api.cryptonator.com/api/ticker/btc-usd"
+    message = ""
+
+    s = sched.scheduler(time.time, time.sleep)
+
+    def __init__(self):
+        auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+        auth.set_access_token(self.access_token, self.access_token_secret)
+        self.api = tweepy.API(auth)
+        user = self.api.me()
+        print(user.name)
+
+        # schedule event to run in 5 minutes
+        self.s.enter(300, 1, self.send_update)
+        self.s.run()
+
+    def send_update(self):
+        self.get_price_data()
+        self.tweet(self.message)
+        self.s.enter(300, 1, self.send_update)  # reschedule event
+
+    def get_price_data(self):
+        response = requests.get(self.api_url)
+        bitcoin_data = json.loads(response.text)
+        bitcoin_data = bitcoin_data["ticker"]
+        base = bitcoin_data["base"]
+        target = bitcoin_data["target"]
+        price = float(bitcoin_data["price"])
+        volume = float(bitcoin_data["volume"])
+        self.message = f'{base}-{target}: ${price:.2f}\nVolume: {volume:.2f} BTC'
+
+    def tweet(self, message):
+        self.api.update_status(message)
+
+
+twit = TwitterBot()
